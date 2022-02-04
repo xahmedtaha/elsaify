63506f13c822beac599c7451bb932894d75ced27
Ahmed Taha <xahmedtaha930@gmail.com>
2022-02-04 20:42:18 +0200

    Add Categorization Logic

--- dist/js/990.aa4569bb.js
+++ dist/js/990.aa4569bb.js
@@ -3,4 +3,4 @@
  * (C) Ionic http://ionicframework.com - MIT License
  */
 const s=(e,t,n,s,c)=>{const i=e.ownerDocument.defaultView,o=e=>e.startX<=50&&t(),u=e=>{const t=e.deltaX,n=t/i.innerWidth;s(n)},l=e=>{const t=e.deltaX,n=i.innerWidth,a=t/n,s=e.velocityX,o=n/2,u=s>=0&&(s>.2||e.deltaX>o),l=u?1-a:a,d=l*n;let h=0;if(d>5){const e=d/Math.abs(s);h=Math.min(e,540)}c(u,a<=0?.01:(0,r.d)(0,a,.9999),h)};return(0,a.createGesture)({el:e,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:o,onStart:n,onMove:u,onEnd:l})}}}]);
-//# sourceMappingURL=990.bad52454.js.map
+//# sourceMappingURL=990.aa4569bb.js.map
