08c6e3ce97005e3e40543b4d94b5079fc1a8b5d2
Ahmed Taha <xahmedtaha930@gmail.com>
2022-03-12 18:54:40 +0200

    Added drawing canvas in NoNetworkPage

--- src/views/NoNetworkPage.vue
+++ src/views/NoNetworkPage.vue
@@ -16,22 +16,38 @@
                     <ion-text v-if="userId" color="primary" class="code">
                         <h3>{{ userId }}</h3>
                     </ion-text>
+                    <svg id="draw-container" ref="target"></svg>
                 </div>
             </div>
+            <ion-note class="draw-note">شغبط هنا لحد ما النت يرجع</ion-note>
         </ion-content>
     </ion-page>
 </template>
 
 <script>
-import { IonPage, IonContent, IonIcon, IonText, IonFabButton } from "@ionic/vue";
+import { IonPage, IonContent, IonIcon, IonText, IonFabButton, IonNote } from "@ionic/vue";
 import { alertCircle, refresh } from 'ionicons/icons';
-import { computed } from 'vue-demi';
+import { computed, onMounted } from 'vue'
+import { createDrauu } from 'drauu'
+import { useCssVar } from '@vueuse/core'
 export default {
     setup() {
-        const userId = computed(() => localStorage.getItem('user_id'));
+        const userId = computed(() => localStorage.getItem('user_id'))
+        onMounted(() => {
+            createDrauu({
+                el: '#draw-container',
+                brush: {
+                    mode: 'stylus', // 'line', 'reactangle', 'ellipse'
+                    color: useCssVar('--ion-color-primary').value,
+                    size: 1.5,
+                }
+            })
+        })
+
         return {
             alertCircle, refresh,
-            userId
+            userId,
+            
         }
     },
     components: {
@@ -40,11 +56,24 @@ export default {
         IonIcon,
         IonText,
         IonFabButton,
+        IonNote,
     },
 };
 </script>
 
 <style scoped>
+#draw-container {
+    background: transparent;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    touch-action: none;
+}
 .code {
     letter-spacing: 3px;
     font-weight: bold;
@@ -56,6 +85,7 @@ export default {
     justify-content: center;
     width: 100%;
     height: 100%;
+    z-index: -2;
 }
 .wrapper {
     max-width: 500px;
@@ -66,6 +96,7 @@ export default {
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    z-index: 0;
 }
 .wrapper {
     width: 100%;
@@ -86,5 +117,13 @@ ion-item {
 }
 ion-icon.warning {
     margin-bottom: 10px;
+}
+.draw-note {
+    text-align: center;
+    position: absolute;
+    bottom: 30px;
+    left: 0;
+    right: 0;
+    z-index: 0;
 }
 </style>
